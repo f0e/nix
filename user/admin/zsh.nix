510d25aaf2372bbaf2dbfb80d7b56b22b1238d9d
f0e <7321764+f0e@users.noreply.github.com>
2025-09-29 21:36:25 +1000

    fix: zsh option deprecation

--- user/admin/zsh.nix
+++ user/admin/zsh.nix
@@ -1,6 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.zsh = {
-    initExtraFirst = ''
+    initContent = lib.mkBefore ''
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
       # confirmations, etc.) must go above this block; everything else may go below.
