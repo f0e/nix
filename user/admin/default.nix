c3818eac4b0f5bd19167a2619b7fe36146c8daf6
f0e <7321764+f0e@users.noreply.github.com>
2025-09-29 22:51:08 +1000

    feat: direnv

--- user/admin/default.nix
+++ user/admin/default.nix
@@ -1,6 +1,7 @@
 {...}: {
   imports = [
     ./1password.nix
+    ./direnv.nix
     ./ghostty.nix
     ./packages.nix
     ./hyprpaper.nix
---
+++ user/admin/direnv.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  # https://github.com/iniw/system/blob/master/modules/terminal/direnv.nix
+  programs.direnv = {
+    enable = true;
+
+    nix-direnv.enable = true;
+
+    config.global = {
+      hide_env_diff = true;
+      strict_env = true;
+    };
+
+    # Store direnv caches in a unified location
+    # From: https://github.com/direnv/direnv/wiki/Customizing-cache-location#human-readable-directories
+    stdlib =
+      # sh
+      ''
+        : "''${XDG_CACHE_HOME:="''${HOME}/.cache"}"
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
+          local hash path
+          echo "''${direnv_layout_dirs[$PWD]:=$(
+            hash="$(${lib.getExe' pkgs.coreutils "sha1sum"} - <<< "$PWD" | head -c40)"
+            path="''${PWD//[^a-zA-Z0-9]/-}"
+            echo "''${XDG_CACHE_HOME}/direnv/layouts/''${hash}''${path}"
+          )}"
+        }
+      '';
+  };
+
+  programs.git.ignores = [".envrc"];
+}
