d33cc4644f4bb56f5341d993572d2c9c02acc08b
f0e <7321764+f0e@users.noreply.github.com>
2025-09-29 21:21:30 +1000

    feat: remove compose on caps key

--- user/default/hyprland/input.nix
+++ user/default/hyprland/input.nix
@@ -4,10 +4,6 @@
     # https://wiki.hyprland.org/Configuring/Variables/#input
     input = lib.mkDefault {
       kb_layout = "us";
-      # kb_variant =
-      # kb_model =
-      kb_options = "compose:caps";
-      # kb_rules =
 
       follow_mouse = 1;
 
