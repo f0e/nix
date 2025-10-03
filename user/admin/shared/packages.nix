9c212ddafe8804e6528e48b6dc4f5e7e4f37e56f
f0e <7321764+f0e@users.noreply.github.com>
2025-10-04 03:29:41 +1000

    feat: lots of shell stuff

--- user/admin/shared/packages.nix
+++ user/admin/shared/packages.nix
@@ -3,22 +3,14 @@
     ffmpeg-full
     btop
 
-    # todo: dedupe, move to system if it makes sense
-    zoxide
-    bat
-    eza
     fd
     sd
     delta
-    ripgrep
     jless
-    fzf
-    antidote
     gron
     curlie
     hyperfine
     hexyl
-    atuin
     csvlens
     vivid
     pastel
--- user/admin/shared/shell.nix
+++ user/admin/shared/shell.nix
@@ -1,21 +1,81 @@
-{...}: {
-  programs.fish = {
-    enable = true;
+{
+  config,
+  home,
+  ...
+}: let
+  dotfiles = "${config.home.homeDirectory}/nix/dotfiles"; # todo: store in centralised config at root level with other shit
+in {
+  programs = {
+    fish = {
+      enable = true;
 
-    interactiveShellInit = ''
-      # enable uv completions if using
-      if type -q uv
-          uv generate-shell-completion fish | source
-      end
-    '';
+      interactiveShellInit = ''
+        # enable uv completions if using
+        if type -q uv
+            uv generate-shell-completion fish | source
+        end
+      '';
+    };
+
+    zsh = {
+      initContent = ''
+        if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
+        then
+            exec fish -l
+        fi
+      '';
+    };
+
+    zoxide = {
+      enable = true;
+
+      options = ["--cmd cd"];
+
+      enableFishIntegration = true;
+    };
+
+    atuin = {
+      enable = true;
+
+      flags = ["--disable-up-arrow"];
+
+      enableFishIntegration = true;
+    };
+
+    bat = {
+      enable = true;
+    };
+
+    eza = {
+      enable = true;
+
+      enableFishIntegration = true;
+    };
+
+    fzf = {
+      enable = true;
+
+      enableFishIntegration = true;
+    };
+
+    jq = {
+      enable = true;
+    };
+
+    ripgrep = {
+      enable = true;
+    };
+
+    starship = {
+      enable = true;
+
+      enableFishIntegration = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+    };
   };
 
-  programs.zsh = {
-    initContent = ''
-      if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
-      then
-          exec fish -l
-      fi
-    '';
+  xdg.configFile = {
+    "starship.toml".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/starship.toml";
   };
 }
