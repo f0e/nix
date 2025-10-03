05694b6d5303215d00b061b2d2df53bafac6908c
f0e <7321764+f0e@users.noreply.github.com>
2025-10-03 17:15:53 +1000

    feat: disable more annoying stuff in vscode

--- user/base/shared/vscode.nix
+++ user/base/shared/vscode.nix
@@ -41,7 +41,10 @@
 
         chat.disableAIFeatures = true;
 
-        biome.suggestInstallingGlobally = false; # annoying
+        # disable annoying stuff
+        biome.suggestInstallingGlobally = false;
+        cmake.showConfigureWithDebuggerNotification = false;
+        editor.inlayHints.enabled = "offUnlessPressed";
       };
 
       keybindings = [
@@ -53,6 +56,43 @@
         {
           key = "ctrl+shift+tab";
           command = "workbench.action.previousEditor";
+        }
+        # cmd+number to switch tabs
+        {
+          key = "cmd+1";
+          command = "workbench.action.openEditorAtIndex1";
+        }
+        {
+          key = "cmd+2";
+          command = "workbench.action.openEditorAtIndex2";
+        }
+        {
+          key = "cmd+3";
+          command = "workbench.action.openEditorAtIndex3";
+        }
+        {
+          key = "cmd+4";
+          command = "workbench.action.openEditorAtIndex4";
+        }
+        {
+          key = "cmd+5";
+          command = "workbench.action.openEditorAtIndex5";
+        }
+        {
+          key = "cmd+6";
+          command = "workbench.action.openEditorAtIndex6";
+        }
+        {
+          key = "cmd+7";
+          command = "workbench.action.openEditorAtIndex7";
+        }
+        {
+          key = "cmd+8";
+          command = "workbench.action.openEditorAtIndex8";
+        }
+        {
+          key = "cmd+9";
+          command = "workbench.action.openEditorAtIndex9";
         }
       ];
     };
