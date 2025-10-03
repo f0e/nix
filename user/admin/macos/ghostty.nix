e848008c46e022d7d3c7bc8d702588aee682aed7
f0e <7321764+f0e@users.noreply.github.com>
2025-10-04 03:29:53 +1000

    feat: ghostty native titlebar style

--- user/admin/macos/ghostty.nix
+++ user/admin/macos/ghostty.nix
@@ -3,12 +3,12 @@
     package = pkgs.ghostty-bin;
 
     settings = {
-      # macos-titlebar-style = "tabs"
+      macos-titlebar-style = "native";
       macos-icon = "custom-style";
-      # macos-icon-frame = "plastic"
+      # macos-icon-frame = "plastic";
       macos-icon-ghost-color = "ffffff";
       macos-icon-screen-color = "000000";
-      # macos-option-as-alt = true
+      # macos-option-as-alt = true;
 
       window-colorspace = "display-p3";
     };
