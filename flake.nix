c8b726886d04a475b53063f82972f2fb74b0e784
f0e <7321764+f0e@users.noreply.github.com>
2025-10-05 19:47:30 +1000

    feat: define desktop arch

--- flake.nix
+++ flake.nix
@@ -64,6 +64,7 @@
   in {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         specialArgs = {inherit inputs;};
         modules =
           [
